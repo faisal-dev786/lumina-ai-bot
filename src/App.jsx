090122ec6ab614df11564ecef78e8210394d8316
Faisal <fasinadem108@gmail.com>
2024-03-26 22:41:05 +0500

    root classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[white] pb-12">
       {" "}
       <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
+      <div className="relative">
+        <p
+          style={{
+            top: "-53px",
+            left: "25px",
+            background: "white",
+          }}
+          className="primary-para pt-10 pb-2 absolute px-1"
+        >
+          Town
+        </p>
+        <input
+          className="p-[15px] w-full border rounded-xl outline-none"
+          placeholder="Town name"
+          type="text"
+          maxLength={25}
+        />
+      </div>
     </div>
   );
 };
