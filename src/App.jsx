0b29814ad073a18b7cd66f4baffd4315674b9494
Faisal <fasinadem108@gmail.com>
2024-03-27 15:09:03 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Signup from "./assets/components/auth/Signup";
+import Navbar from "./assets/components/Navbar";
 const App = () => {
   return (
-    <div className="bg-[white] pb-12">
-      {" "}
-      <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
